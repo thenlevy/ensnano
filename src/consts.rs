3e6a59186f0e61daee5a3f9bf71f37ffcd23d494
Nicolas Levy <nlevy_als@hotmail.fr>
2021-10-15 17:11:27 +0200

    Draw correctly the cube on widows at startup

--- src/consts.rs
+++ src/consts.rs
@@ -112,7 +112,7 @@ pub const WELCOME_MSG: &str = "
 ==============================================================================
                                WELCOME TO ENSNANO\n
 During runtime, the console may print error messages that are useful to the
-to the programer to investigate bugs.\n
+programer to investigate bugs.\n
 However, because of a current bug in wgpu, some error messages may be prompted
 that could be safely ignored.
 Please ignore messages of the following form:\n
