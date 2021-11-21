e46dd87685edbfeb0efa7b548f9c21e617907fbe
nicolas <nicolas.schabanel@ens-lyon.fr>
2021-11-21 21:36:54 +0100

    code affreux mais ça marche

--- ensnano-design/src/curves/torus.rs
+++ ensnano-design/src/curves/torus.rs
@@ -113,9 +113,183 @@ impl Torus {
         }
     }
 
-    // MOEBIUS RING
+    // Moebius
 
-    fn ellipse_perimeter_approximation(a: f32, b: f32) -> f32 {
+    fn perimeter_ellipse(&self, a: f32, b: f32, nb_steps: usize) -> f32 {
+        let mut p = 0f32;
+        let mut u = Vec2 { x: a, y: 0. };
+        for i in 0..nb_steps+1 {
+            let t = TAU * i as f32 / nb_steps as f32;
+            let v = Vec2 { x: a * t.cos(), y: b * t.sin() };
+            p += (v - u).mag();
+            u = v;
+        }
+        p
+    }
+
+    fn t_for_curvilinear_abscissa(&self, s: f32) -> f32 {
+        let p = 9.688448061179066_f32;
+        let perimeter = 4. * H * self.half_nb_helix as f32;
+        let scale = perimeter / p;
+        let mut sp = s / scale;        
+        let a = 2.;
+        let b = 1.;
+        while sp < 0. { 
+            sp += p;
+        }
+        while sp > p {
+            sp -= p;
+        }
+        let nb_steps = NB_STEPS;
+        let mut u = Vec2 { x: a, y: 0. };
+        let mut t = 0f32;
+        for i in 0..nb_steps+1 { // SHOULD COMPUTE A CHEBYCHEB POLY APPROX
+            if sp <= 0. {
+                break;
+            }
+            t = TAU * i as f32 / nb_steps as f32;
+            let v = Vec2 { x: a * t.cos(), y: b * t.sin() };
+            sp -= (v - u).mag();
+            u = v;
+        }
+        t
+    }
+
+    fn t_for_curvilinear_abscissa_poly(&self, s: f32) -> f32 {
+        let p = 9.688448061179066_f32;
+        let perimeter = 4. * H * self.half_nb_helix as f32;
+        let scale = perimeter / p;
+        let coef: [f32; 21] = [0.00012918397789041247, 0.1515814901975501, 0.10450751273807285, -0.6649830252676487, 1.7278194213623754, -2.8339254809794006, 3.1496843695687855, -2.466968925697648, 1.4018887658135728, -0.5905631979287363, 0.18721564526394163, -0.04507183747440176, 0.008268531828443176, -0.0011525981861020874, 0.00012080470580909873, -9.318521725420798e-06, 5.085225494171747e-07, -1.8187171826245956e-08, 3.5515542659526737e-10, -1.404395246708242e-12, -4.930219196343138e-14];
+
+        let mut sp = s / scale;
+        while sp < 0. {
+            sp += p;
+        }
+        while sp >= p {
+            sp -= p;
+        }
+        let mut result = 0_f32;
+        for i in (0..coef.len()).rev() {
+            result = sp * result + coef[i];
+        }
+        return TAU * result
+    }
+
+    fn position_Moebius(&self, t:f32) -> Vec3 {
+        let p = 9.688448061179066_f32;
+        let perimeter = 4. * H * self.half_nb_helix as f32;
+        // println!("p: {}\t P: {}\tφ:{}\tφφ:{}", perimeter, self.perimeter_ellipse(2.,1., NB_STEPS), self.t_for_curvilinear_abscissa_poly(perimeter/2.), self.t_for_curvilinear_abscissa(perimeter/2.));
+        let scale = perimeter / p;
+        let a = 2. * scale;
+        let b = 1. * scale;
+        let theta = self.theta(t) - self.theta0;
+        let theta_dt = self.theta_dt();
+        let s_dtheta = (perimeter / 2. + 4. * H) / TAU; 
+        let s = 4. * H * self.theta0 / TAU + s_dtheta * theta;
+        let phi = self.t_for_curvilinear_abscissa(s);
+        let (t2c, t2s) = ((theta / 2.).cos(), (theta / 2.).sin());
+        let (pc, ps) = (phi.cos(), phi.sin());
+        let (x,y) = (a * phi.cos(), b * phi.sin());
+        Vec3 {
+            x: (x * t2c - y * t2s + self.big_radius) * theta.cos(),
+            y: x * t2s + y * t2c,
+            z: (x * t2c - y * t2s + self.big_radius) * theta.sin(),
+        }
+    }
+
+    fn speed_Moebius(&self, t:f32) -> Vec3 {
+        let dt = 1. / NB_STEPS as f32;
+        let x = self.position_Moebius(t);
+        let x_dx = self.position_Moebius(t+dt);
+        return (x_dx - x) / dt;
+
+        let p = 9.688448061179066_f32;
+        let perimeter = 4. * H * self.half_nb_helix as f32;
+        let scale = perimeter / p;
+        let a = 2. * scale;
+        let b = 1. * scale;
+        let theta = self.theta(t) - self.theta0;
+        let theta_dt = self.theta_dt();
+        let s_dtheta = H + (perimeter / 2. + 4. * H) / TAU; 
+        let s = s_dtheta * theta;
+        let phi = self.t_for_curvilinear_abscissa(s);
+        let (t2c, t2s) = ((theta / 2.).cos(), (theta / 2.).sin());
+        let (pc, ps) = (phi.cos(), phi.sin());
+        let (x,y) = (a * phi.cos(), b * phi.sin());
+        let n_dt = (a*a*ps*ps + b*b*pc*pc).sqrt() / theta_dt / s_dtheta;
+        let (x_dt, y_dt) = (- a * ps / n_dt, b * pc / n_dt);
+        Vec3 {
+            x: theta_dt * ( 
+                - (x * t2c - y * t2s + self.big_radius) * theta.sin()
+                + (x_dt * t2c - x * t2s / 2. - y_dt * t2s - y * t2c / 2.) * theta.cos()
+            ), // diff((X(t)*cos(t/2)-Y(t)*sin(t/2)+R)*cos(t),t) = 1/2 (-2 R sin(t) + cos(t) (2 cos(t/2) X'(t) + X(t) (-sin(t/2)) - 2 sin(t/2) Y'(t) - Y(t) cos(t/2)) - 2 sin(t) (X(t) cos(t/2) - Y(t) sin(t/2)))
+            y: theta_dt * (
+                x_dt * t2s + x * t2c / 2. + y_dt * t2c - y * t2s / 2.
+            ), // diff((X(t)*sin(t/2)+Y(t)*cos(t/2)),t) = d/dt(X(t) sin(t/2) + Y(t) cos(t/2)) = sin(t/2) X'(t) + 1/2 X(t) cos(t/2) + cos(t/2) Y'(t) - 1/2 Y(t) sin(t/2)
+            z: theta_dt * ( 
+                (x * t2c - y * t2s) * theta.cos()
+                + (x_dt * t2c - x * t2s / 2. - y_dt * t2s - y * t2c / 2.) * theta.sin()
+            ), // diff((X(t)*cos(t/2)-Y(t)*sin(t/2))*sin(t),t) = sin(t) cos(t/2) X'(t) - 1/2 X(t) sin(t/2) sin(t) + X(t) cos(t/2) cos(t) - sin(t/2) sin(t) Y'(t) - Y(t) sin(t/2) cos(t) - 1/2 Y(t) sin(t) cos(t/2)
+        }
+    }
+
+    fn acceleration_Moebius(&self, t: f32) -> Vec3 {
+        Vec3 {
+            x: 0.,
+            y: 0.,
+            z: 1.,
+        }
+    }
+    
+}
+
+impl Curved for Torus {
+    fn position(&self, t: f32) -> Vec3 {
+        return self.position_Moebius(t);
+    }
+
+    fn speed(&self, t: f32) -> Vec3 {
+        return self.speed_Moebius(t);
+    }
+    
+    fn acceleration(&self, t: f32) -> Vec3 {
+        return self.acceleration_Moebius(t);
+    }
+}
+
+
+
+
+
+
+// MOEBIUS RING
+
+const NB_STEPS: usize = 1_000;
+
+pub struct Ellipse {
+    pub a: f32,
+    pub b: f32,
+}
+
+
+impl Ellipse {
+    fn position(&self, t: f32) -> Vec2 {
+        Vec2 {
+            x: self.a * (TAU * t).cos(),
+            y: self.b * (TAU * t).sin(), 
+        }
+    }
+
+    fn speed(&self, t: f32) -> Vec2 {
+        Vec2 {
+            x: -TAU * self.a * (TAU * t).sin(),
+            y: TAU * self.b * (TAU * t).cos(), 
+        }
+    }
+
+    fn perimeter_approximation(&self) -> f32 {
+        let a = self.a;
+        let b = self.b;
         let h_ = (a - b)/ (a + b);
         let h = h_ * h_;
         let p = PI * (a + b) * (1. + 3. * h / (10. + (4. - 3. * h).sqrt()));
@@ -123,85 +297,136 @@ impl Torus {
         return p;
     }
 
-    fn ellipse_parameters(&self, ratio_a_b: f32, n: usize) -> (f32, f32) {
-        let b_ = 1 as f32;
-        let a_ = ratio_a_b;
-        let p = Self::ellipse_perimeter_approximation(a_, b_);
-
-        let wanted_p = 4. * n as f32 * H;
-        let x = wanted_p / p;
-
-        return (a_ * x, b_ * x);
+    fn perimeter(&self, nb_steps: usize) -> f32 {
+        let mut p = 0f32;
+        let mut u = self.position(0.);
+        for i in 0..nb_steps+1 {
+            let t = i as f32 / nb_steps as f32;
+            let v = self.position(t);
+            p += (v - u).mag();
+            u = v;
+        }
+        p
     }
 
-    fn position_moebius_ring(&self, t: f32) -> Vec3 {
-        let theta = self.theta(t);
-        let small_radius = self.small_radius();
-        let phi = self.phi(t);
+    fn scale_for(&self, half_nb_helix: usize) -> f32 {
+        let p = self.perimeter(NB_STEPS);
+        let desired_p = 4. * H * half_nb_helix as f32;
+        desired_p / p
+    }
+}
 
-        Vec3 {
-            z: theta.cos() * (self.big_radius + small_radius * phi.cos()),
-            x: theta.sin() * (self.big_radius + small_radius * phi.cos()),
-            y: phi.sin() * small_radius,
+pub struct MoebiusRing {
+    pub curve: Ellipse,
+    pub perimeter: f32,
+    pub half_nb_helix: usize,
+    pub scale: f32,
+    pub nb_steps: usize,
+    pub big_radius: f32,
+    pub theta0: f32,
+}
+
+impl MoebiusRing {
+    pub fn new(curve: Ellipse, half_nb_helix: usize, big_radius: f32, theta0: f32) -> Self {
+        let p = curve.perimeter(NB_STEPS);
+        let s = curve.scale_for(half_nb_helix);
+        Self {
+            curve: curve,
+            perimeter: p,
+            half_nb_helix: half_nb_helix,
+            scale: s,
+            nb_steps: NB_STEPS,
+            big_radius: big_radius,
+            theta0: theta0,
         }
     }
 
-    // Rotating rectangle
-    fn n1(&self) -> usize { self.half_nb_helix & 255 } // number of helices on the horizontal side
-    fn n2(&self) -> usize { self.half_nb_helix >> 8 } // number of helices on the vertical side
-    fn n12(&self) -> usize { self.n1() + self.n2() }
-    
-    fn side1(&self) -> f32 { (self.n1() - 1) as f32 * 2. * H }
-    fn side2(&self) -> f32  { (self.n2() - 1) as f32 * 2. * H }
-    fn rectangle_perimeter(&self) -> f32 { 2. * (self.side1() + self.side2()) }
+    fn t_for_curvilinear_abscissa(&self, s: f32) -> f32 {
+        let mut sp = s / self.scale;
+        while sp < 0. { 
+            sp += self.perimeter;
+        }
+        while sp > self.perimeter {
+            sp -= self.perimeter;
+        }
+        let nb_steps = self.nb_steps;
+        let ds = 1. / nb_steps as f32;
+        let mut u = self.curve.position(0.);
+        let mut t = 0f32;
+        for i in 0..nb_steps+1 { // SHOULD COMPUTE A CHEBYCHEB POLY APPROX
+            if sp <= 0. {
+                break;
+            }
+            t = i as f32 / nb_steps as f32;
+            let v = self.curve.position(t);
+            sp -= (v - u).mag();
+            u = v;
+        }
+        t
+    }
 
-    fn t1(&self) -> f32 { self.side1() / self.rectangle_perimeter() }
-    fn t2(&self) -> f32 { (self.side1() + self.side2()) / self.rectangle_perimeter() }
-    fn t3(&self) -> f32 { (2. * self.side1() + self.side2()) / self.rectangle_perimeter() }
+    fn theta(&self, t: f32) -> f32 {
+        TAU * (2. * self.half_nb_helix as f32) * t / 2. + self.theta0
+    }
 
-    fn rectangle(&self, t: f32) -> Vec2 {
-        let (s1, s2) = (self.side1(), self.side2());
-        let mut s = t * 2. * (s1 + s2);
+    fn theta_dt(&self) -> f32 {
+        TAU * (2. * self.half_nb_helix as f32) / 2.
+    }
 
-        let c0 = Vec2 { x: s1/2., y: s2/2., };
-        let c1 = Vec2 { x: -s1/2., y: s2/2., };
-        let c2 = Vec2 { x: -s1/2., y: -s2/2., };
-        let c3 = Vec2 { x: s1/2., y: -s2/2., };
+    fn position_Moebius(&self, t:f32) -> Vec3 {
+        let theta = self.theta(t);
+        let theta_dt = self.theta_dt();
+        let perimeter = self.perimeter * self.scale;
+        let s_dtheta = (self.perimeter / 2. + 4. * H) / TAU; 
+        let s = s_dtheta * theta;
+        let phi = self.t_for_curvilinear_abscissa(s);
+        let a = self.curve.a * self.scale;
+        let b = self.curve.b * self.scale;
+        let (t2c, t2s) = ((theta / 2.).cos(), (theta / 2.).sin());
+        let (pc, ps) = (phi.cos(), phi.sin());
+        let (x,y) = (a * phi.cos(), b * phi.sin());
+        Vec3 {
+            x: (x * t2c - y * t2s + self.big_radius) * theta.cos(),
+            y: x * t2s + y * t2c,
+            z: (x * t2c - y * t2s + self.big_radius) * theta.sin(),
+        }
+    }
 
-        if s < s1 {
-            let u = s / s1;
-            return c0 * (1. - u) + u * c1;
-        } 
-        s -= s1;
-        if s < s2 {
-            let u = s / s2;
-            return c1 * (1. - u) + u * c2;
-        } 
-        s -= s2; 
-        if s < s1  {
-            let u = s / s1;
-            return c2 * (1. - u) + u * c3;
-        } 
-        s -= s1;
-        if s <= s2 {
-            let u = s / s2;
-            return c3 * (1. - u) + u * c0;
-        } 
-        return c0
+    fn speed_Moebius(&self, t:f32) -> Vec3 {
+        let theta = self.theta(t);
+        let theta_dt = self.theta_dt();
+        let s_dtheta = (self.perimeter / 2. + 4. * H) / TAU; 
+        let s = s_dtheta * theta;
+        let phi = self.t_for_curvilinear_abscissa(s);
+        let a = self.curve.a * self.scale;
+        let b = self.curve.b * self.scale;
+        let (t2c, t2s) = ((theta / 2.).cos(), (theta / 2.).sin());
+        let (pc, ps) = (phi.cos(), phi.sin());
+        let (x,y) = (a * phi.cos(), b * phi.sin());
+        let n_dt = (a*a*ps*ps + b*b*pc*pc).sqrt() / theta_dt / s_dtheta;
+        let (x_dt, y_dt) = (- a * ps / n_dt, b * pc / n_dt);
+        Vec3 {
+            x: theta_dt * ( 
+                - (x * t2c - y * t2s + self.big_radius) * theta.sin()
+                + (x_dt * t2c - x * t2s / 2. - y_dt * t2s - y * t2c / 2.) * theta.cos()
+            ), // diff((X(t)*cos(t/2)-Y(t)*sin(t/2)+R)*cos(t),t) = 1/2 (-2 R sin(t) + cos(t) (2 cos(t/2) X'(t) + X(t) (-sin(t/2)) - 2 sin(t/2) Y'(t) - Y(t) cos(t/2)) - 2 sin(t) (X(t) cos(t/2) - Y(t) sin(t/2)))
+            y: theta_dt * (
+                x_dt * t2s + x * t2c / 2. + y_dt * t2c - y * t2s / 2.
+            ), // diff((X(t)*sin(t/2)+Y(t)*cos(t/2)),t) = d/dt(X(t) sin(t/2) + Y(t) cos(t/2)) = sin(t/2) X'(t) + 1/2 X(t) cos(t/2) + cos(t/2) Y'(t) - 1/2 Y(t) sin(t/2)
+            z: theta_dt * ( 
+                (x * t2c - y * t2s) * theta.cos()
+                + (x_dt * t2c - x * t2s / 2. - y_dt * t2s - y * t2c / 2.) * theta.sin()
+            ), // diff((X(t)*cos(t/2)-Y(t)*sin(t/2))*sin(t),t) = sin(t) cos(t/2) X'(t) - 1/2 X(t) sin(t/2) sin(t) + X(t) cos(t/2) cos(t) - sin(t/2) sin(t) Y'(t) - Y(t) sin(t/2) cos(t) - 1/2 Y(t) sin(t) cos(t/2)
+        }
+    }
+
+    fn acceleration_Moebius(&self, t: f32) -> Vec3 {
+        Vec3 {
+            x: 0.,
+            y: 0.,
+            z: 1.,
+        }
     }
 
 }
 
-impl Curved for Torus {
-    fn position(&self, t: f32) -> Vec3 {
-        return self.position_torus(t);
-    }
-
-    fn speed(&self, t: f32) -> Vec3 {
-        return self.speed_torus(t);
-    }
-    
-    fn acceleration(&self, t: f32) -> Vec3 {
-        return self.acceleration_torus(t);
-    }
-}
