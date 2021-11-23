3c0641f5c86cd84247146b392ec48df150932c5c
nicolas <nicolas.schabanel@ens-lyon.fr>
2021-11-23 08:30:34 +0100

    mod.rs en f64

--- ensnano-design/src/curves/mod.rs
+++ ensnano-design/src/curves/mod.rs
@@ -16,8 +16,8 @@ ENSnano, a 3d graphical application for DNA nanostructures.
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-use ultraviolet::{Mat3, Vec3};
-const EPSILON: f32 = 1e-6;
+use ultraviolet::{DMat3, DVec3};
+const EPSILON: f64 = 1e-6;
 const DISCRETISATION_STEP: usize = 100;
 use super::{Helix, Parameters};
 use std::sync::Arc;
@@ -32,15 +32,15 @@ use torus::TwistedTorus;
 pub use torus::{CurveDescriptor2D, TwistedTorusDescriptor};
 pub use twist::Twist;
 
-const EPSILON_DERIVATIVE: f32 = 1e-3;
+const EPSILON_DERIVATIVE: f64 = 1e-6;
 pub(super) trait Curved {
-    fn position(&self, t: f32) -> Vec3;
-    fn speed(&self, t: f32) -> Vec3 {
+    fn position(&self, t: f64) -> DVec3;
+    fn speed(&self, t: f64) -> DVec3 {
         (self.position(t + EPSILON_DERIVATIVE / 2.) - self.position(t - EPSILON_DERIVATIVE / 2.))
             / EPSILON_DERIVATIVE
     }
 
-    fn acceleration(&self, t: f32) -> Vec3 {
+    fn acceleration(&self, t: f64) -> DVec3 {
         ((self.position(t + EPSILON_DERIVATIVE) + self.position(t - EPSILON_DERIVATIVE))
             - 2. * self.position(t))
             / (EPSILON_DERIVATIVE * EPSILON_DERIVATIVE)
@@ -49,22 +49,22 @@ pub(super) trait Curved {
 
 pub(super) struct Curve {
     geometry: Box<dyn Curved + Sync + Send>,
-    positions: Vec<Vec3>,
-    axis: Vec<Mat3>,
+    positions: DVec<DVec3>,
+    axis: DVec<DMat3>,
 }
 
 impl Curve {
     pub fn new<T: Curved + 'static + Sync + Send>(geometry: T, parameters: &Parameters) -> Self {
         let mut ret = Self {
             geometry: Box::new(geometry),
-            positions: Vec::new(),
-            axis: Vec::new(),
+            positions: DVec::new(),
+            axis: DVec::new(),
         };
         ret.discretize(parameters.z_step, DISCRETISATION_STEP);
         ret
     }
 
-    pub fn length_by_descretisation(&self, t0: f32, t1: f32, nb_step: usize) -> f32 {
+    pub fn length_by_descretisation(&self, t0: f64, t1: f64, nb_step: usize) -> f64 {
         if t0 < 0. || t1 > 1. || t0 > t1 {
             log::error!(
                 "Bad parameters ofr length by descritisation: \n t0 {} \n t1 {} \n nb_step {}",
@@ -74,9 +74,9 @@ impl Curve {
             );
         }
         let mut p = self.geometry.position(t0);
-        let mut len = 0f32;
+        let mut len = 0f64;
         for i in 1..=nb_step {
-            let t = t0 + (i as f32) / (nb_step as f32) * (t1 - t0);
+            let t = t0 + (i as f64) / (nb_step as f64) * (t1 - t0);
             let q = self.geometry.position(t);
             len += (q - p).mag();
             p = q;
@@ -84,20 +84,20 @@ impl Curve {
         len
     }
 
-    fn discretize(&mut self, len_segment: f32, nb_step: usize) {
+    fn discretize(&mut self, len_segment: f64, nb_step: usize) {
         let len = self.length_by_descretisation(0., 1., nb_step);
         let nb_points = (len / len_segment) as usize;
-        let small_step = 1. / (nb_step as f32 * nb_points as f32);
+        let small_step = 1. / (nb_step as f64 * nb_points as f64);
 
         let mut points = Vec::with_capacity(nb_points + 1);
         let mut axis = Vec::with_capacity(nb_points + 1);
-        let mut t = 0f32;
+        let mut t = 0f64;
         points.push(self.geometry.position(t));
         let mut current_axis = self.itterative_axis(t, None);
         axis.push(current_axis);
 
         while t < 1. {
-            let mut s = 0f32;
+            let mut s = 0f64;
             let mut p = self.geometry.position(t);
 
             while s < len_segment {
@@ -116,12 +116,12 @@ impl Curve {
         self.positions = points;
     }
 
-    fn itterative_axis(&self, t: f32, previous: Option<&Mat3>) -> Mat3 {
+    fn itterative_axis(&self, t: f64, previous: Option<&DMat3>) -> DMat3 {
         let speed = self.geometry.speed(t);
         if speed.mag_sq() < EPSILON {
             let acceleration = self.geometry.acceleration(t);
             let mat = perpendicular_basis(acceleration);
-            return Mat3::new(mat.cols[2], mat.cols[1], mat.cols[0]);
+            return DMat3::new(mat.cols[2], mat.cols[1], mat.cols[0]);
         }
 
         if let Some(previous) = previous {
@@ -129,7 +129,7 @@ impl Curve {
             let up = forward.cross(previous.cols[0]).normalized();
             let right = up.cross(forward);
 
-            Mat3::new(right, up, forward)
+            DMat3::new(right, up, forward)
         } else {
             perpendicular_basis(speed)
         }
@@ -139,14 +139,14 @@ impl Curve {
         self.positions.len()
     }
 
-    pub fn axis_pos(&self, n: usize) -> Option<Vec3> {
+    pub fn axis_pos(&self, n: usize) -> Option<DVec3> {
         self.positions.get(n).cloned()
     }
 
-    pub fn nucl_pos(&self, n: usize, theta: f32, parameters: &Parameters) -> Option<Vec3> {
+    pub fn nucl_pos(&self, n: usize, theta: f64, parameters: &Parameters) -> Option<DVec3> {
         if let Some(matrix) = self.axis.get(n).cloned() {
             let mut ret = matrix
-                * Vec3::new(
+                * DVec3::new(
                     -theta.cos() * parameters.helix_radius,
                     theta.sin() * parameters.helix_radius,
                     0.,
@@ -158,27 +158,27 @@ impl Curve {
         }
     }
 
-    pub fn points(&self) -> &[Vec3] {
+    pub fn points(&self) -> &[DVec3] {
         &self.positions
     }
 }
 
-fn perpendicular_basis(point: Vec3) -> Mat3 {
+fn perpendicular_basis(point: DVec3) -> DMat3 {
     let norm = point.mag();
 
     if norm < EPSILON {
-        return Mat3::identity();
+        return DMat3::identity();
     }
 
     let axis_z = point.normalized();
 
-    let mut axis_x = Vec3::unit_x();
+    let mut axis_x = DVec3::unit_x();
     if axis_z.x >= 1. - EPSILON {
-        axis_x = Vec3::unit_y();
+        axis_x = DVec3::unit_y();
     }
     axis_x = (axis_x.cross(axis_z)).normalized();
 
-    Mat3::new(axis_x, axis_x.cross(-axis_z), axis_z)
+    DMat3::new(axis_x, axis_x.cross(-axis_z), axis_z)
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
